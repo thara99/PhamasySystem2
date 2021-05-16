354722d604e4c022a8d354fa820722dcdcaf97e6
thara99 <it19148014@my.sliit.lk>
2021-05-16 21:36:09 +0530

    commit3

---
+++ PhamasySystem2/Migrations/20210516154451_MyMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhamasySystem2.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: true),
+                    Disease = table.Column<string>(nullable: true),
+                    Amount = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicines", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicines");
+        }
+    }
+}
---
+++ PhamasySystem2/Migrations/Customer/20210516155040_MyMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhamasySystem2.Migrations.Customer
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CId = table.Column<Guid>(nullable: false),
+                    CName = table.Column<string>(nullable: true),
+                    PNum = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    NIC = table.Column<string>(nullable: true),
+                    AllergyMedicines = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
